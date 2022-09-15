6adb1fc0db22be083c2274af82fbd5b08cd2b360
Resad Memmedov <resadmemmedov0035@gmail.com>
2022-09-15 21:06:31 +0400

    refactor: Remove unnecessary comments

--- src/corePackages/OniCore.Persistence/Dynamic/Sort.cs
+++ src/corePackages/OniCore.Persistence/Dynamic/Sort.cs
@@ -5,32 +5,4 @@
         public string Field { get; set; } = string.Empty;
         public string Direction { get; set; } = string.Empty;
     }
-
-    /*
-     * {
-     *  field: "color",
-     *  operator: "eq",
-     *  value: "green",
-     *  and: [
-     *   {
-     *    field: "cpu",
-     *    operator: "eq",
-     *    value: "i3",
-     *    or: [
-     *     {
-     *      field: "cpu",
-     *      operator: "eq",
-     *      value: "i5",
-     *     },
-     *     {
-     *      field: "cpu",
-     *      operator: "eq",
-     *      value: "i7",
-     *     }
-     *    ]
-     *   }
-     *  ]
-     * }    
-     * 
-     */
 }
